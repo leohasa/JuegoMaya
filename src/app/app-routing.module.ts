0de821bdb0cbca84cebece6d2a02c180ee6ca233
Julio <juliofernando-ixcoy@cunoc.edu.gt>
2022-04-19 20:51:48 -0600

    opciones en el juego de matematicas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MathematicsComponent } from './mathematics/mathematics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'math', component: MathematicsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
